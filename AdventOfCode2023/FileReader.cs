d8c696554944e80b3cb70749388294e2d67189b6
HGabor93 <gabor.heizer@gmail.com>
2023-12-04 18:07:50 +0100

    FileReader tidy up

--- AdventOfCode2023/FileReader.cs
+++ AdventOfCode2023/FileReader.cs
@@ -9,31 +9,12 @@ namespace AdventOfCode2023
     internal class FileReader
     {
 
-        public static string[] ReadFileArray(string filePath, string day)
-        {
-            string fullFilePath = filePath + @"\day" + day + ".txt";
-
-            string[] lines = System.IO.File.ReadAllLines(fullFilePath);
-
-            // Display the file contents by using a foreach loop.
-            System.Console.WriteLine("Contents: ");
-            foreach (string line in lines)
-            {
-                // Use a tab to indent each line of the file.
-                Console.WriteLine("\t" + line);
-            }
-
-            return lines;
-
-        }
-
         public static List<string> ReadFileList(string filePath, string day)
         {
             string fullFilePath = filePath + @"\day" + day + ".txt";
 
             // Read each line of the file into a string array. Each element
-            // of the array is one line of the file.
-            //(@"C:\Users\Public\TestFolder\WriteLines2.txt")            
+            // of the array is one line of the file.        
             string[] lines = System.IO.File.ReadAllLines(fullFilePath);
             List<string> newFileContents = new List<string>();
 
